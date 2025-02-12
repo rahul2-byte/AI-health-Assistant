d355a218c789166ba83498e99b7f6a4c5803bf0e
rahul2-byte <rahulchand4299@gmail.com>
2025-02-12 21:25:35 +0530

    Added telegram bot functionality for ai-agent

--- app/components/client.py
+++ app/components/client.py
@@ -1,7 +1,6 @@
 # client.py
 import os
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
+from langchain_community.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 
 def llm():
@@ -12,6 +11,7 @@ def llm():
     # Return LLM Model
     return ChatOpenAI(
         model="deepseek/deepseek-r1:free",
+        # model="openai/gpt-4o-mini",
         openai_api_key=api_key,
         base_url="https://openrouter.ai/api/v1"
     )
--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from components.agents import llmchain
+from components.messanger import Bot
 
 def main():
 
     prompt = "How many grams of protien does chiken have..?"
-    output = llmchain.get_output(prompt)
+    # output = llmchain.get_output(prompt)
+    # print("Final Output :- ", output)
+    
+    bot = Bot()
+    
+    text = bot.run()
 
-    print("Final Output :- ", output)
+    print("Text", text)
 
 if __name__ == "__main__":
     main()
